ca6013644e5e63f42a6d9adbf6af995268c26503
samrasugu <mokuasamr@gmail.com>
2023-10-22 13:07:00 +0300

    feat: initial commit - font scheme setup - color scheme setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'primary-orange': '#F77310',
+        'primary-black': '#1E1E1E',
+        'primary-grey': '#676767',
+        'primary-white': '#F9F9F9'
+      }
     },
   },
   plugins: [],
